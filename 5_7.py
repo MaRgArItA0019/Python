4cb02138b6d0f26fafc3ef448262d080ae0f08c5
Margarita Denisova <denisovamargarita5@gmail.com>
2021-12-27 19:07:54 +0400

    Lesson-5

--- 5_7.py
+++ 5_7.py
@@ -1,8 +1,12 @@
 import json
+
 with open("text_77.json", "w", encoding="utf-8") as j:
     with open("text_7.txt", encoding="utf-8") as f:
         profit = {line.split()[0]: int(line.split()[2]) - int(line.split()[3]) for line in f}
-        average_profit = [profit, {"Средняя прибыль": sum([int(i) for i in
-                                                           profit.values() if int(i) > 0]) / len([int(i)]) for i
-                                   in profit.values() if int(i) > 0}]
-    json.dump(average_profit, j, ensure_ascii=False)
+        average_profit = [profit, {"Средняя прибыль": round(sum([int(i) for i in
+                                                                 profit.values() if int(i) > 0]) / len([int(i) for i
+                                                                                                        in
+                                                                                                        profit.values()
+                                                                                                        if
+                                                                                                        int(i) > 0]))}]
+    json.dump(average_profit, j, ensure_ascii=False, indent=4)
